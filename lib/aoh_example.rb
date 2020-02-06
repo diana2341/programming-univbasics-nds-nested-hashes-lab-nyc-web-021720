9be0f508075b0ca20ae61b05a0acb861f13321f5
Diana Ponce <dianap2341@gmail.com>
2020-02-06 21:58:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,12 +20,13 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  names=[{ name:  "Don Gately", occupation: "Live-in Staff" },
- { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  names=[
+{ name:  "Don Gately", occupation: "Live-in Staff" },
+{ name:  "Joelle van Dyne", occupation: "Radio Personality" },
 { name:  "Pat Monteseian", occupation: "Staff" },
 { name:  "Kate Gompert", occupation: "None" },
- { name:  "Bruce Green", occupation: "Fan of Mildred" }
-].sort
+{ name:  "Bruce Green", occupation: "Fan of Mildred" }
+]
 end
 
 def aoh_lookup(aoh, row, key)
